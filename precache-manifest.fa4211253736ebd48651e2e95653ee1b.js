7d423213df1c167f9881418a03d885d6835bb7f8
Vsevolod <seva34land@mail.ru>
2020-06-07 13:26:02 +0300

    Updates

---
+++ precache-manifest.fa4211253736ebd48651e2e95653ee1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92765f9c81a72565ab3085dc9a49801a",
+    "url": "/Drobyshevskiy/STYLISH_CLOTHES/index.html"
+  },
+  {
+    "revision": "e6b8ca234577253ba5f4",
+    "url": "/Drobyshevskiy/STYLISH_CLOTHES/static/css/main.c0e375ba.chunk.css"
+  },
+  {
+    "revision": "1835b0ad11796377be0b",
+    "url": "/Drobyshevskiy/STYLISH_CLOTHES/static/js/2.c8b5331f.chunk.js"
+  },
+  {
+    "revision": "27f2b29734af216f1b6e94db7058f93f",
+    "url": "/Drobyshevskiy/STYLISH_CLOTHES/static/js/2.c8b5331f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b8ca234577253ba5f4",
+    "url": "/Drobyshevskiy/STYLISH_CLOTHES/static/js/main.33ff830b.chunk.js"
+  },
+  {
+    "revision": "0bc2d22c8a33a75288ef",
+    "url": "/Drobyshevskiy/STYLISH_CLOTHES/static/js/runtime-main.8908bab5.js"
+  }
+]);
